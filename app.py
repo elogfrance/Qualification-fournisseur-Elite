674376ca4385f29824adf90cfa32cab419f12cc1
elogfrance <Mehdi.b@e-log-france.com>
2025-06-19 20:50:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -156,8 +156,8 @@ def afficher_fiche_qualification():
         index=[" ", "Oui", "Non"].index(fiche_existante.get("Poids/volume", " ")) if fiche_existante else 0
     )
     statut_final = st.selectbox(
-        "📌 Statut final", ["✅", "⚠️", "❌"],
-        index=["✅", "⚠️", "❌"].index(fiche_existante.get("Statut final", "✅")) if fiche_existante else 0
+        "📌 Statut final", ["Non qualifiés", "Qualifié", "En cours", "Non éligible à l'Elite""],
+        index=[""Non qualifiés", "Qualifié", "En cours", "Non éligible à l'Elite""].index(fiche_existante.get("Statut final", " ")) if fiche_existante else 0
     )
     commentaire = st.text_area("📝 Commentaire", value=fiche_existante.get("Commentaire", "") if fiche_existante else "")
 
